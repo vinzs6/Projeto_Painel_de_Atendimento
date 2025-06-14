b5b3898b33d4dff8733dfd75d4cf019cbc4afcc1
Wilson-Jose <jwfsn@outlook.com.br>
2025-06-13 23:03:44 -0300

    Algumas alterações que incluem : restauração das funçoes data e hora, correção do footer e reposicionamento de conteudos.

--- assets/JS/footer.js
+++ assets/JS/footer.js
@@ -1,4 +1,47 @@
-function atualizarTopoRodape() {
+function DataHora() {
+  const agora = new Date();
+
+  const data = agora.toLocaleDateString("pt-BR", {
+    // weekday: 'long',
+    year: "numeric",
+    month: "2-digit", // mais padronizado
+    day: "2-digit",
+  });
+
+  const hora = agora.toLocaleTimeString("pt-BR", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  document.getElementById("dataHora").textContent = `${hora} ${data}`;
+  // document.getElementById('hora').textContent = `${hora}`;
+}
+setInterval(DataHora, 1000);
+DataHora();
+
+// Buscar clima usando OpenWeather
+async function buscarClima() {
+  const apiKey = "1cc74e49bdf9f9a0b65fcfc199bf0685"; // key de API do OpenWeather
+  const cidade = "São Paulo"; // Cidade para buscar o clima
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&lang=pt_br&units=metric`;
+
+  try {
+    const resposta = await fetch(url);
+    const dados = await resposta.json();
+    const temperatura = dados.main.temp;
+    const descricao = dados.weather[0].description;
+    document.getElementById(
+      "clima"
+    ).textContent = `${cidade}: ${temperatura.toFixed(1)}°C`;
+  } catch (erro) {
+    document.getElementById("clima").textContent =
+      "Não foi possível carregar o clima.";
+  }
+}
+
+buscarClima();
+
+/*function atualizarTopoRodape() {
   const agora = new Date();
 
   // Formatando data e hora
@@ -22,7 +65,7 @@ function atualizarTopoRodape() {
   if (rodapeData) rodapeData.textContent = textoDataHora;
 
   // Clima via OpenWeather
-  const apiKey = "1cc74e49bdf9f9a0b65fcfc199bf0685"; // ⚠️ IMPORTANTE: Esta chave da API está hardcoded temporariamente.
+  const apiKey = "1cc74e49bdf9f9a0b65fcfc199bf0685"; // Esta chave da API está hardcoded temporariamente.
     // Em versões futuras, será movida para o back-end ou variáveis de ambiente (.env). 
   const cidade = "São Paulo";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&lang=pt_br&units=metric`;
@@ -42,9 +85,7 @@ function atualizarTopoRodape() {
       if (climaTopo) climaTopo.textContent = "-- °C";
       if (climaRodape) climaRodape.textContent = "-- °C";
     });
-}
-
-buscarClima();
+}  */
 
 // SENHA
 let senhaAtual = 1;
